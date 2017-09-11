945687f74a97aaa8c4805d633374ab27a88fe88f
joebutkovic <joebutkovic@gmail.com>
2017-09-11 16:20:46 -0400

    Add files via upload

---
+++ Homework1.r
@@ -0,0 +1,48 @@
+Joseph Butkovic
+
+c.) 
+> 2017^(1/3)
+[1] 12.63481
+
+d.)
+> abs(5.7-6.8)/.58
+[1] 1.896552
+
+e.)
+> a = 1:12
+> a
+ [1]  1  2  3  4  5  6  7  8  9 10 11 12
+
+f.)
+> b = c(1, 3, 5, 7, 9, 11)
+> b
+[1]  1  3  5  7  9 11
+
+g.)
+> c = seq(1,11, 2)
+> c
+[1]  1  3  5  7  9 11
+
+h.)
+> ln.a = log(a)
+> ln.a
+ [1] 0.0000000 0.6931472 1.0986123 1.3862944 1.6094379 1.7917595 1.9459101
+ [8] 2.0794415 2.1972246 2.3025851 2.3978953 2.4849066
+
+i)
+> c = seq(1,11, 2)
+> c
+[1]  1  3  5  7  9 11
+> c^2
+[1]   1   9  25  49  81 121
+
+j.)
+"This function computes the standard deviation of the values in x. If na.rm is TRUE then missing values are removed before computation proceeds." - http://127.0.0.1:17040/library/stats/html/sd.html
+
+k.)
+> Name = "Joe"
+> paste("My name is ", Name)
+[1] "My name is  Joe"  
+
+L.)
+Done.
